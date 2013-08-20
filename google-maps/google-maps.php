b7dc14514fb7ddb694b6506cc3818a48e20e7ece
enkreativbyra <oscar@enkreativbyra.se>
2013-08-20 08:26:54 +0200

    Added author and class name

--- google-maps/google-maps.php
+++ google-maps/google-maps.php
@@ -2,7 +2,11 @@
 /*
 	Plugin Name: Google Maps
 	Version: 1.0
+	Author: Oscar Byhlinder
+	Author URI: http://wunderweb.se
 	PageLines: true
 	Description: Google Maps Section.
+	Class Name: GoogleMaps
+	Demo: 
 	V3:true
 */
